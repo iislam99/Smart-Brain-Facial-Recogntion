bde5f67796ddcfa6cd3f5ba83d90fd4af6fd07c5
Iftekharul Islam <IIslam99@csu.fullerton.edu>
2022-02-13 18:29:59 -0800

    Moved files to Front-End folder.

---
+++ Front-End/src/App.js
@@ -0,0 +1,194 @@
+import React, { Component } from 'react';
+import Particles from 'react-tsparticles';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Leaderboard from "./components/Leaderboard/Leaderboard";
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 40,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      boxes: [],
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }});
+  }
+
+  calculateFaceLocation = (data) => {
+    let boxes = [];
+    const regions = data.outputs[0].data.regions;
+    for (let i = 0; i < regions.length; i++) {
+      let face = regions[i].region_info.bounding_box;
+      let image = document.getElementById('inputimage');
+      let width = Number(image.width);
+      let height = Number(image.height);
+
+      boxes.push({
+        leftCol: face.left_col * width,
+        topRow: face.top_row * height,
+        rightCol: width - (face.right_col * width),
+        bottomRow: height - (face.bottom_row * height)
+      })
+    }
+
+    return boxes;
+  }
+
+  displayFaceBox = (boxes) => {
+    this.setState({boxes: boxes});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('https://rocky-island-07169.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('https://rocky-island-07169.herokuapp.com/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+              id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+          .catch(console.log);
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({
+        isSignedIn: false,
+        imageUrl: ''
+      });
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
+  render () {
+    const { isSignedIn, imageUrl, route, boxes, user } = this.state;
+    return (
+      <div className="App">
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {
+          route === 'home'
+            ? <div>
+                <Logo />
+                <Rank
+                  name={user.name}
+                  entries={user.entries}
+                />
+                <ImageLinkForm
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+                
+              </div>
+            : (
+                route === 'signin' || route === 'signout'
+                  ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                  : (
+                      route === 'leaderboard'
+                      ? <Leaderboard />
+                      : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                  )
+              )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
